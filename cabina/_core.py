c1cb23791689463d5d204b0f454b2bf61799ef8a
Юрий Сагитов <yu.sagitov@2gis.ru>
2025-06-25 11:39:59 +0300

    dynamic updated environ env with futured resolution added

--- cabina/_core.py
+++ cabina/_core.py
@@ -17,6 +17,7 @@ from typing import (
 
 from niltype import Nil, NilType
 
+from ._future_transparent_value import FutureTransparentValue
 from ._future_value import FutureValue
 from .errors import (
     ConfigAttrError,
@@ -181,6 +182,8 @@ class MetaBase(type):
         """
         attr = super().__getattribute__(name)
         if isinstance(attr, FutureValue):
+            return attr.get()
+        if isinstance(attr, FutureTransparentValue):
             return attr.get()
         return attr
 
